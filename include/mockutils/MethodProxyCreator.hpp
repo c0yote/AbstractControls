cfa8c39b1fcdc175940be3086bed5b2fdf43c53f
U.G. Wilson <ugwilson@gmail.com>
2015-06-11 20:32:31 -0500

    Squashed 'dep/FakeIt/' content from commit 0b73dcd
    git-subtree-dir: dep/FakeIt
    git-subtree-split: 0b73dcd0c3d6b29419e27dc4000d03c6132e6a85

---
+++ include/mockutils/MethodProxyCreator.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "mockutils/VirtualTable.hpp"
+#include "mockutils/MethodProxy.hpp"
+#include "mockutils/Destructible.hpp"
+
+namespace fakeit {
+
+    struct InvocationHandlerCollection {
+        static const unsigned int VT_COOKIE_INDEX = 0;
+
+        virtual Destructible *getInvocatoinHandlerPtrById(unsigned int index) = 0;
+
+        static InvocationHandlerCollection *getInvocationHandlerCollection(void *instance) {
+            VirtualTableBase &vt = VirtualTableBase::getVTable(instance);
+            InvocationHandlerCollection *invocationHandlerCollection = (InvocationHandlerCollection *) vt.getCookie(
+                    InvocationHandlerCollection::VT_COOKIE_INDEX);
+            return invocationHandlerCollection;
+        }
+    };
+
+
+    template<typename R, typename ... arglist>
+    class MethodProxyCreator {
+
+        //using MethodProxyPtrType = R(MethodProxyCreator::*)(arglist...);
+
+    public:
+
+        template<unsigned int id>
+        MethodProxy createMethodProxy(unsigned int offset) {
+            return MethodProxy(id, offset, union_cast<void *>(&MethodProxyCreator::methodProxyX < id > ));
+        }
+
+    protected:
+
+        R methodProxy(unsigned int id, arglist &... args) {
+            InvocationHandlerCollection *invocationHandlerCollection = InvocationHandlerCollection::getInvocationHandlerCollection(
+                    this);
+            MethodInvocationHandler<R, arglist...> *invocationHandler =
+                    (MethodInvocationHandler<R, arglist...> *) invocationHandlerCollection->getInvocatoinHandlerPtrById(
+                            id);
+            return invocationHandler->handleMethodInvocation(args...);
+        }
+
+        template<int id>
+        R methodProxyX(arglist ... args) {
+            return methodProxy(id, args...);
+        }
+    };
+}
